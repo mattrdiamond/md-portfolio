11a239ea7b424d56b363a2b64a5cb06410aaae2a
Matt Diamond <mattdiamond80@gmail.com>
2019-09-14 11:33:06 -0500

    initial commit

---
+++ src/scripts/Fart.js
@@ -0,0 +1,12 @@
+// export default class Fart {
+//   constructor() {
+//     this.test = "test";
+//   }
+
+//   loud() {
+//     console.log("brrrrap!");
+//   }
+// }
+export default function fart() {
+  alert("brrrap!");
+}
