f3be5fc93a8ffff75ce5eb3c64dca4087d80ce95
Matt Diamond <mattdiamond80@gmail.com>
2019-09-16 17:24:26 -0500

    Update typing speed

--- src/scripts/TypeWriter.js
+++ src/scripts/TypeWriter.js
@@ -37,7 +37,7 @@ class TypeWriter {
       this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
 
       // Initial Type Speed
-      let typeSpeed = 300;
+      let typeSpeed = 250;
 
       if (this.isDeleting) {
         typeSpeed /= 2;
