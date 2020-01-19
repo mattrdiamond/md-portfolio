3fc5f1addc05bfd64546812c1aeabe2167ee96ac
Matt Diamond <mattdiamond80@gmail.com>
2020-01-19 11:35:46 -0600

    Add load screen

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,3 +5,8 @@ import slideUp from "./slideUp";
 import landingPage from "./landingPage";
 import footer from "./footer";
 import missionStatement from "./missionStatement";
+
+window.onload = () => {
+  const loader = document.getElementById("load-screen");
+  loader.classList.add("loaded");
+};
