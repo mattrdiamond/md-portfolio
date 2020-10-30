830eb12bda9894f99acc787aec5a99e59443a797
Matt Diamond <mattdiamond80@gmail.com>
2020-10-30 16:43:46 -0500

    Move tooltip text to data attribute

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -5,49 +5,57 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: {
-    app: Path.resolve(__dirname, "../src/scripts/index.js")
+    app: Path.resolve(__dirname, "../src/scripts/index.js"),
   },
   output: {
     path: Path.join(__dirname, "../build"),
-    filename: "js/[name].js"
+    filename: "js/[name].js",
   },
   optimization: {
     splitChunks: {
       chunks: "all",
-      name: false
-    }
+      name: false,
+    },
   },
   plugins: [
     new CleanWebpackPlugin(),
     new CopyWebpackPlugin([
-      // { from: Path.resolve(__dirname, '../public'), to: 'public' }
-      { from: Path.resolve(__dirname, "../src/img"), to: "img" } // copy images folder to build folder
+      // copy images folder to build folder
+      { from: Path.resolve(__dirname, "../src/img"), to: "img" },
+      // include CNAME
+      { from: Path.resolve(__dirname, "../CNAME") },
     ]),
     new HtmlWebpackPlugin({
-      template: Path.resolve(__dirname, "../src/index.html")
-    })
+      template: Path.resolve(__dirname, "../src/index.html"),
+    }),
   ],
   resolve: {
     alias: {
-      "~": Path.resolve(__dirname, "../src")
-    }
+      "~": Path.resolve(__dirname, "../src"),
+    },
   },
   module: {
     rules: [
       {
         test: /\.mjs$/,
         include: /node_modules/,
-        type: "javascript/auto"
+        type: "javascript/auto",
       },
       {
         test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)(\?.*)?$/,
         use: {
           loader: "file-loader",
           options: {
-            name: "[path][name].[ext]"
-          }
-        }
-      }
-    ]
-  }
+            name: "[path][name].[ext]",
+          },
+        },
+      },
+      {
+        test: /CNAME$/,
+        use: {
+          loader: "file-loader",
+        },
+      },
+    ],
+  },
 };
